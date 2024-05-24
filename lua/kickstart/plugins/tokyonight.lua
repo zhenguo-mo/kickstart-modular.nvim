e3f4605914ba7aca4a6f7960ccdb33e1bb265af9
Zhen Mo <zhengumo@amazon.com>
2024-05-24 15:10:06 -0700

    Update transparency and add suggested plugins

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -15,6 +15,9 @@ return {
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
+    opts = {
+      transparent = true,
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
